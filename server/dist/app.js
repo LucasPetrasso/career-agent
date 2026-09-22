a8115685871d39d27eb1d2fb8c737ffa6362a8c1
Lucas Petrasso <lucaspetrasso@hotmail.com>
2026-09-22 19:05:45 -0300

    Estrutura inicial concluida

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
+import { corsOptions } from "./config/cors.js";
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.get("/api/health", (request, response) => {
     response.status(200).json({
